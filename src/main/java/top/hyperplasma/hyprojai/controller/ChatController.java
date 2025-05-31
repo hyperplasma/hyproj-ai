45e2e9f85de2968cbe77899bf0e4483e1d923c07
Akira37 <863781751@qq.com>
2025-05-31 18:45:50 +0800

    Update chat functionality to include chatId and adjust memory configuration

--- src/main/java/top/hyperplasma/hyprojai/controller/ChatController.java
+++ src/main/java/top/hyperplasma/hyprojai/controller/ChatController.java
@@ -2,6 +2,7 @@ package top.hyperplasma.hyprojai.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -14,10 +15,10 @@ public class ChatController {
     private final ChatClient chatClient;
 
     @RequestMapping(value = "/chat", produces = "text/html;charset=utf-8")
-    public Flux<String> chat(String prompt) {
+    public Flux<String> chat(String prompt, String chatId) {
         return chatClient.prompt()
                 .user(prompt)
-                .advisors()
+                .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, chatId))
                 .stream()
                 .content();
     }
